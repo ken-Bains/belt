e46218f76073b833b51a4f5deb7d8fb89ac326dd
Ken Bains <kennbains@gmail.com>
2016-12-15 17:12:57 -0800

    updates

--- apps/mainApp/models.py
+++ apps/mainApp/models.py
@@ -6,111 +6,80 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]')
 
 # Create your models here.
 class userManager(models.Manager):
-	def register(self, email, password, first=None, alias=None, passwordCheck=None, form=None):
+	def register(self, post):
+		first = post['first_name']
+		last  = post['last_name']
+		email = post['email']
+		password = post['password']
+		passwordCheck = post['passwordCheck']
 		errors = []
 		userId = 0
+
+		if len(first) < 2:
+			errors.append('first name must be longer than 2 characters')
+
+		if len(last) < 2:
+			errors.append('last name must be longer than 2 characters')
+
 		if len(email) < 1:
-			errors.append('email cannot be blank!')
+			errors.append('email cannot be blank')
 		elif not EMAIL_REGEX.match(email):
-			errors.append('email cannot be blank!')
+			errors.append('email not in right format')
 		
 		if len(password) < 8:
 			errors.append('password needs to be grater than 8 characters')
 
-		#if login form has no errors then check password and set session id. 
-		if len(errors) == 0 and form != 'registration':
-			user = User.objects.filter(email = email)
-			hashed = user[0].password
-
-			if bcrypt.hashpw(password.encode(), hashed.encode()) == hashed:
-				# request.session['id'] = user[0].id
-				userId = user[0].id
+		if password != passwordCheck:
+			errors.append('password does not match')
 
 
-		# check to see if coming from registration page
-		if form == "registration":
-			if len(first) < 2:
-				errors.append('first name must be longer than 2 characters')
-
-			if len(alias) < 2:
-				errors.append('alias name must be longer than 2 characters')
-
-			if password != passwordCheck:
-				errors.append('password does not match')
-			
-			checkIfEmailExist = User.objects.filter(email=email)
-
-			if len(checkIfEmailExist) > 0:
-				errors.append('invalid')
-
-			if len(errors) == 0:
+		if not errors:
+			emailExist = User.objects.filter(email=email)
+			if not emailExist:
 				hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-				user = User.objects.create(email=email, first_name= first, alias=alias, password=hashed)
+				user = User.objects.create(email=email, first_name= first, last_name=last, password=hashed)
 				userId = user.id
+			else:
+				errors.append('Invalid login')
+		
+		return [errors, userId]
+
+	def login(self, post):
+		email = post['email']
+		password = post['password']
+		errors = []
+		userId = 0
+		
+		if len(email) < 1:
+			errors.append('email cannot be blank!')
+		elif not EMAIL_REGEX.match(email):
+			errors.append('email not in right format')
+		
+		if len(password) < 8:
+			errors.append('password needs to be grater than 8 characters')
+		
+		if not errors:
+			emailExist = User.objects.filter(email=email)
+
+			if emailExist:
+				hashed = emailExist[0].password
+
+				if bcrypt.hashpw(password.encode(), hashed.encode()) == hashed:
+					userId = emailExist[0].id
+			else:
+				errors.append('Invalid login')
+
 
 		return [errors, userId]
 
 class User(models.Model):
 	first_name = models.CharField(max_length=50)
-	alias = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
 	email = models.CharField(max_length=100)
 	password = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add = True)
 	updated_at = models.DateTimeField(auto_now = True)
 	objects = userManager()
-
-#
-#
-#      ---------end of user-------
-#
-
-class bookManager(models.Manager):
-
-	def add_book(self, post, user_id):
-		title = post['book_title']
-		author = post['author']
-	
-		check_if_book_exist = Book.objects.filter(title = title)
-		
-		if not check_if_book_exist:
-			book_object = Book.objects.create(title = title, author=author)
-		else:
-			res = {"status": False, "error": "book already exist"}
-			return res
-		
-		Book.objects.add_review(post, user_id, book_object)
-		res = {"status": True, "book_id":book_object.id}
-		return res 
-	
-	def add_review(self, post, user_id, book=None):
-		review = post['review']
-		rating = post['rating']
-		if book == None:
-			book_object = Book.objects.filter(id = post['book_id'])
-			book = book_object[0]
-		user_object = User.objects.filter(id = user_id)
-		user = user_object[0]
-		
-		Review.objects.create(rating=rating, review=review, user=user, book=book)
-
-		return book
-
-class Book(models.Model):
-	title = models.CharField(max_length=50)
-	author = models.CharField(max_length=50)
-	created_at = models.DateTimeField(auto_now_add = True)
-	objects = bookManager()
-
-
-
-class Review(models.Model):
-	rating = models.SmallIntegerField()
-	review = models.TextField(max_length=1000)
-	created_at = models.DateTimeField(auto_now_add = True)
-	user = models.ForeignKey('User')
-	book = models.ForeignKey('Book')
-	objects = bookManager()
-
 
 
 
--- apps/mainApp/views.py
+++ apps/mainApp/views.py
@@ -6,88 +6,38 @@ from django.contrib import messages
 
 
 def index(request):
-	
 	return render(request, 'mainApp/index.html')
 
 def newUser(request):
-	first = request.POST['first_name']
-	alias  = request.POST['alias']
-	email = request.POST['email']
-	password = request.POST['password']
-	passwordCheck = request.POST['passwordCheck']
-	form = "registration"
-
-	res = models.User.objects.register(email, password, first, alias, passwordCheck, form)
-
-	if len(res[0]) == 0:
-		messages.success(request, "You have successfully logged in!!!!")
-		request.session['id'] = res[1]
-		return redirect('/books')
-
-	for error in res[0]:
-		messages.warning(request, error)
-
-	return redirect('/')
+	res = models.User.objects.register(request.POST)
+	message = "registered"
+	return processSignon(request, res, message)
 
 def signOn(request):
-	email = request.POST['email']
-	password = request.POST['password']
+	res = models.User.objects.login(request.POST)
+	message = "logged on"
+	return processSignon(request, res, message)
 
-	res = models.User.objects.register(email, password)
-
-	if len(res[0]) == 0:
-		messages.success(request, "You have successfully logged in!!!!")
+def processSignon(request, res, message):	
+	if not res[0]:
+		messages.success(request, "You have successfully {}!!!!".format(message))
 		request.session['id'] = res[1]
-		return redirect('/books')
+		return redirect('/success')
 
 	for error in res[0]:
 		messages.warning(request, error)
-
 	return redirect('/')
 
-def all_books(request):
-	if request.session.get('id'): #'id' not in request.session:
+def success(request):
+	if request.session.get('id'):
 		userInfo = models.User.objects.filter(id = request.session['id'])
-		all_reviews = models.Review.objects.all().order_by('-created_at')
-		reviews = [all_reviews[0], all_reviews[1], all_reviews[2]]
-		data = {"user":userInfo[0], "reviews":reviews, "all_reviews": all_reviews}
-		return render(request, "mainApp/all_books.html", data)
+		data = {"user":userInfo[0]}
+		return render(request, "mainApp/success.html", data)
 	return redirect('/')
 
-def logout(request):
-	request.session.clear()
-	return redirect('/')
 
-def book_add_page(request):
-	if request.session.get('id'):
-		return render(request, 'mainApp/book_add.html')
-	return redirect('/')
 
-def one_book(request, id):
-	if request.session.get('id'):
-		book = models.Book.objects.filter(id=id)
-		reviews = models.Review.objects.filter(book__id = id)
-		data = {"book":book[0], "reviews": reviews}
-		return render(request, 'mainApp/one_book.html', data)
-	return redirect('/')
 
-def add_new_book(request):
-	res = models.Book.objects.add_book(request.POST, request.session['id'])
-	if res['status'] == False:
-		messages.warning(request, res['error'])
-		return redirect ('/book/add')
-	return redirect('/book/{}'.format(res['book_id']))
 
-def user_info_page(request, id):
-	if request.session.get('id'):
-		all_reviews = models.Review.objects.filter(user__id = request.session['id'])
-		reviews = [all_reviews[0], all_reviews[1], all_reviews[2]]
-		user = all_reviews[0]
-		data = { "reviews":reviews, "all_reviews": all_reviews, "user":user}
-		return render(request, 'mainApp/user_info.html', data)
-	return redirect('/')
 
-def add_review(request):
-	res = models.Book.objects.add_review(request.POST, request.session['id'])
-	return redirect('/book/{}'.format(res.id))
 
